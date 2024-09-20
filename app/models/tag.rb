429fb825696f73098d25cdf6da24d23b16c95240
Victor Horton <vichorton5@gmail.com>
2024-09-20 17:18:20 -0500

    Add simple seeds for tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,10 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :training_resources
+  before_save :downcase_name
+
+  private
+
+  def downcase_name
+    self.name = self.name.downcase
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
+10.times do
+  Tag.create(
+    name: Faker::Company.buzzword
+  )
+end
+
 100.times do
   TrainingResource.create(
     author: Faker::Name.name,
     title: Faker::Company.catch_phrase,
     short_description: Faker::Lorem.sentence,
-    long_description: Faker::Lorem.paragraph
+    long_description: Faker::Lorem.paragraph,
+    tag_ids: Faker::Number.between(from: 1, to: 10)
   )
 end
