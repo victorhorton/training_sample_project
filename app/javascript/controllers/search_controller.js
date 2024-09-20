fd7b0720b2e90ff964586ffade78d50fdae6fe07
Victor Horton <vichorton5@gmail.com>
2024-09-19 23:11:32 -0500

    Add search bar

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,11 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["select"]; // Define targets if necessary
+  static targets = ["input"]; // Define targets if necessary
 
-  setPagination(event) {
+  setSearch(event) {
     const searchParams = new URLSearchParams(window.location.search);
-    searchParams.set("per_page", event.target.value);
+    searchParams.set("search", this.inputTarget.value);
     window.location.replace(
       location.origin + location.pathname + "?" + searchParams.toString()
     );
