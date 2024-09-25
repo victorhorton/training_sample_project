38b18b91bb35d107af9dfad5bac59fb307c89fe6
Victor Horton <vichorton5@gmail.com>
2024-09-24 20:42:32 -0500

    Create users table with secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_24_025844) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_25_013817) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,6 +60,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_025844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "youtube_video_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.integer "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
