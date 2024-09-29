18fd5dcc590fd1392b4a820ded56a90540e3e513
Victor Horton <vichorton5@gmail.com>
2024-09-29 15:42:07 -0500

    Improve unauthorized handling and notices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
     return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    if exception.subject == TrainingResource
+      redirect_to login_path, notice: "Please login"
+    else
+      redirect_to root_path, notice: exception.message
+    end
+  end
 end
