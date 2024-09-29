16fd68535b1547ee0df0278375a622e51a0976c4
Victor Horton <vichorton5@gmail.com>
2024-09-29 18:08:06 -0500

    Add computerphile resources to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@ code_aesthetic_data.each_with_index do |row, idx|
     short_description: row['short_description'],
     long_description: row['long_description'],
     featured_image: File.open(Rails.root.join("db/seeds_data/#{row['featured_image_path']}")),
-    youtube_video_id: row['youtube_video_id']
+    youtube_video_id: row['youtube_video_id'],
+    duration: row['duration']
   )
 
   row['tag_names'].each do |tag|
@@ -16,14 +17,21 @@ code_aesthetic_data.each_with_index do |row, idx|
   end
 end
 
-tags_data = YAML.load(File.open(Rails.root.join("db/seeds_data/tags.yaml")))
-tags_data.each do |row|
-  Tag.find_or_create_by!(row.to_hash)
-end
+computerphile_data = YAML.load(File.open(Rails.root.join("db/seeds_data/computerphile/training_resources.yaml")))
+computerphile_data.each_with_index do |row, idx|
+  training_resource = TrainingResource.create(
+    title: row['title'],
+    author: row['author'],
+    short_description: row['short_description'],
+    long_description: row['long_description'],
+    featured_image: File.open(Rails.root.join("db/seeds_data/#{row['featured_image_path']}")),
+    youtube_video_id: row['youtube_video_id'],
+    duration: row['duration']
+  )
 
-training_resources_data = YAML.load(File.open(Rails.root.join("db/seeds_data/training_resources.yaml")))
-training_resources_data.each_with_index do |row, idx|
-  TrainingResource.create(row.to_hash)
+  row['tag_names'].each do |tag|
+    training_resource.tags << Tag.find_or_create_by!(name: tag.downcase)
+  end
 end
 
 users_data = YAML.load(File.open(Rails.root.join("db/seeds_data/users.yaml")))
