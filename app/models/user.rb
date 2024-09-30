bfb40da3789f86992b1121552399b9aeb7b121f1
Victor Horton <vichorton5@gmail.com>
2024-09-29 23:03:10 -0500

    Seperate update user info and user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,18 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, uniqueness: true, presence: true
-  validates :password, length: { minimum: 8 }, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 8 }, confirmation: true, if: :password_changing
+  validates :password_confirmation, presence: true, if: :password_changing
 
   before_create :set_default_role
 
   enum role: [ :admin, :level_1 ]
 
   private
+
+  def password_changing
+    self.password.present?
+  end
 
   def set_default_role
     if role.nil?
